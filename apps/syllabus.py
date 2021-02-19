6dedee1019a1034d9739d0b831916c1962fa2b60
Sean Robinson <seankrobinson@users.noreply.github.com>
2021-02-18 20:28:49 -0500

    Adds markdown for syllabus

--- apps/syllabus.py
+++ apps/syllabus.py
@@ -9,7 +9,132 @@ def app():
     st.write("This is a sample home page in the mutliapp.")
     st.write("See `apps/home.py` to know how to use it.")
 
-    st.markdown("### Sample Data")
+    st.markdown("””
+    # University of North Carolina at Charlotte School of Data Science
+
+## DSBA 6520, HCIP 6520, ITIS 6520: Network Science
+### Summer I & II 2021
+### Sean K. Robinson
+### Contact Information:
+Email: srobi116@uncc.edu
+Twitter: @sean_k_robinson
+Office Hours: End of Class and via [Calendly](calendly.com/seankrobinson)
+Slack: dsba6520summer2021.slack.com
+
+### Course
+-  Tuesdays
+-  May 24 – Aug. 9, 2021
+-  5:30-9:15 PM
+-  Zoom Link
+-  Slack
+
+### Course Description 
+Throughout our lives, we encounter data with various degrees of structure which can be relied upon when attempting to make systematic or algorithmic evaluations of that data. And it is the structure (or lack thereof) of that data which determines what algorithms or operations can be performed on said data, and by extension, what insights can be gained. 
+
+In this class, you’ll learn how to use industry-standard algorithms, tools, and models which take advantage of data which is structured as a graph/network as well as the rules and theory which govern Network Science to uncover hidden interconnected insights in data.
+
+### Course Objectives
+In this course, you will 
+1.  Learn to wield the tools and technologies which are used to implement network science techniques 
+2.  Implement graph analytics and machine learning on real data
+3.  Understand how graph theory and algorithms compliment  traditional data science
+4.  Gain hands-on experience taking exiting flat data and discovering the hidden graphs and information within it
+5.  Develop a substantial graph-based portfolio project 
+
+
+### Course Schedule (Subject to Change as Needed)
+
+| Date | Topic | Due Before Class |
+|:--|:--|:--|
+| 5/25 | Intro to Graphs & Networks |  |
+| 6/1 |  NO CLASS | Class Survey |
+| 6/8 | Graphs & Networks cont. / Graph Algorithms| Homework 1 |
+| 6/15 | Graph Algorithms cont. / Graph Databases | Project Deliverable I |
+| 6/22 | Graph Databases cont.| Homework II |
+| 6/29 | Graph Visualization | Project Deliverable II |
+| 7/6 | Exam, Introduction to Graph Machine Learning |  |
+| 7/13 | Graph Machine Learning cont. | Project Deliverable III |
+| 7/20 | Utilization of Network Science | Homework III |
+| 7/27 | Open Week | Project Deliverable IV | 
+| 8/3 | Presentations | 
+| 8/9 |  | Final Project Due |
+
+### Syllabus Subject to Change
+The standards and requirements set forth in this syllabus may be modified by the course instructor. Notice of such changes will be made in advance and by announcement in class.
+
+### Course Materials
+The course books are optional to purchase as the textbooks have free online versions available. Although, I’d still highly recommend buying physical copies (but it’s completely optional).
+
+The recommended books are:
+
+- Graph Algorithms, Mark Needham & Amy Hodler, Published by O'Reilly Media, [FREE [online](https://neo4j.com/graph-algorithms-book) or ~ $40 new at [Amazon](https://www.amazon.com/Graph-Algorithms-Practical-Examples-Apache/dp/1492047686/ref=nodl_)]
+- The Atlas for the Aspiring Network Scientist, Michele Castr, [FREE [online](https://www.networkatlas.eu/) or ~ $40 new at [Amazon](https://www.amazon.com/s?k=9788797282403&i=stripbooks&linkCode=qs)]
+
+
+
+### Tools & Technologies 
+Throughout this class we will use a variety of tools to interact with networks and graphs at every level. From diagraming data, to database storage, to visualization and machine learning. Students are welcome to use any technologies which allow them to fully complete the project prompts. There are two exceptions to this rule which are required and detailed below.
+#### draw.io
+[draw.io](draw.io) will be used to submit all graph data modeling  and designs for this class. Students are welcome to design via pen & paper or a whiteboard to formulate ideas but all final submissions to canvas must be done through draw.io diagrams for the purposes of cleanliness and uniformity.
+#### Neo4J
+While Neo4J and its adjacent plugins can be used for the entirety of a project, it is only required to be used for the database section of the course. Should students choose to use alternative technologies for other sections of the course, those technologies should be compatible with Neo4J and source any data used as such.
+
+### Online help and Slack
+Computer programming can be difficult. Computers are stupid and little errors in your code can cause hours of headache (even if you’ve been doing this stuff for years!).
+
+Fortunately there are tons of online resources to help you with this. One the most important is StackOverflow (a Q&A site with hundreds of thousands of answers to all sorts of programming questions)
+
+Additionally, we have a class chatroom on Slack where anyone in the class can ask questions and anyone can answer. Ask questions about the readings, problem sets, and projects in the class Slack workspace. I will monitor Slack regularly, and you should also all do so as well. You’ll likely have similar questions as your peers, and you’ll likely be able to answer other peoples’ questions too.
+
+#### Order of operations for having your question answered
+1. Google it
+2. Reference Product Documentation 
+3. Ask on Slack
+4. Attend Office Hours
+5. Email Me
+
+### Classroom Policies
+#### Be Nice. Be Honest. Ask Questions. Don’t Cheat.
+
+Orderly and productive classroom conduct
+I will conduct this class in an atmosphere of mutual respect. I encourage your active participation in class discussions. Each of us may have strongly differing opinions on the various topics of class discussions. The conflict of ideas is encouraged and welcome. The orderly questioning of the ideas of others, including mine, is similarly welcome. However, I will exercise my responsibility to manage the discussions so that ideas and argument can proceed in an orderly fashion. You should expect that if your conduct during class discussions seriously disrupts the atmosphere of mutual respect I expect in this class, you will not be permitted to participate further.
+
+### Recording in the classroom
+Electronic video and/or audio recording is not permitted during class unless the student obtains permission from the instructor. If permission is granted, any distribution of the recording is prohibited. Students with specific electronic recording accommodations authorized by the Office of Disability Services do not require instructor permission; however, the instructor must be notified of any such accommodation prior to recording. Any distribution of such recordings is prohibited. Video recordings of each lecture may be posted after each class for students to review.
+
+### Discussion of grades and performance
+Such discussion shall occur between the student and the instructor(s). Sharing information regarding grades and performance in places such as discussion forums or email blasts is prohibited.
+
+### Code of Student Responsibility
+“The purpose of the Code of Student Responsibility (the Code) is to protect the campus community and to maintain an environment conducive to learning. University rules for student conduct are discussed in detail. The procedures followed for any Student, Student Organization or Group charged with a violation of the Code, including the right to a hearing before a Hearing Panel or Administrative Hearing Officer, are fully described.” (Introductory statement from the UNC Charlotte brochure about the Code of Student Responsibility). The entire document may be found at this site: https://legal.uncc.edu/policies/up-406
+
+### Academic Integrity
+All students are required to read and abide by the Code of Student Academic Integrity. Violations of the Code of Student Academic Integrity, including plagiarism, will result in disciplinary action as provided in the Code. Students are expected to submit their own work, either as individuals or contributors to a group assignment. Definitions and examples of plagiarism and other violations are set forth in the Code. The Code is available from the Dean of Students Office or online at: https://legal.uncc.edu/policies/up-407.
+
+Faculty may ask students to produce identification at examinations and may require students to demonstrate that graded assignments completed outside of class are their own work.
+
+### Assignments and Grades
+You can find descriptions for all the assignments on the assignments page.
+#### Project-Based Class Structure
+The assignments and project deliverables for this class are designed to build on top of one another as we progress through the class. As such, keeping up with homework assignments and project deliverables will be critical to successfully completing the final project. Students who choose to procrastinate assignments or not fully learn a given section of the course may find themselves in a scramble as the course progresses. However, students who regularly engage in class and with the coursework will find that many of the assignments fulfill a portion of the following assignment; making it easy to stay on top of and manage upcoming work.
+
+#### Extra Credit
+Extra Credit can be earned in one of two ways
+1. Students can earn up to 25 points (2.5%) by committing their work to GitHub and submitting their project deliverables with a link to the GitHub project included
+2. Students may also earn up to 100 points (10%) on their final project submission through in-depth, effective demonstration of network science techniques. Further details will be provided in class. 
+
+| Assignment | Points (+Extra Credit)| % of Total Grade |
+|:--|:--|:--|
+| Homeworks | 50 | 5% |
+| Midterm | 200 | 20% |
+| Final Exam | 200 | 20% |
+| Project Deliverable I | 50 | 5% |
+| Project Deliverable II | 100 (+5) | 10% |
+| Project Deliverable III | 100 (+5) | 10% |
+| Project Deliverable IV | 100 (+5) | 10% |
+| Final Project | 200 (+100)(+10) | 20% |
+    
+    “”")
     df = create_table()
     st.write(df)
 
