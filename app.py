02a28cb2991bb233072aa6665dd2edd3c76d1303
Sean Robinson <Seankrobinson95@gmail.com>
2021-02-18 21:23:37 -0500

    Adds course pages to app.py

--- app.py
+++ app.py
@@ -1,12 +1,13 @@
 import streamlit as st
 from multiapp import MultiApp
-from apps import home, data_stats # import your app modules here
+from apps import home, assignments, schedule, syllabus # import your app modules here
 
 app = MultiApp()
 
 # Add all your application here
 app.add_app("Home", home.app)
-app.add_app("Data Stats", data_stats.app)
-
+app.add_app("assignments", assignments.app)
+app.add_app("schedule", schedule.app)
+app.add_app("syllabus", syllabus.app)
 # The main app
 app.run()
