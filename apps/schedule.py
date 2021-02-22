c52a935c881fa439be62c64945448b4ed0a9d413
Sean Robinson <Seankrobinson95@gmail.com>
2021-02-22 10:50:35 -0500

    Adding class logo and moving schedule CSV to assets dir

--- apps/schedule.py
+++ apps/schedule.py
@@ -3,7 +3,7 @@ import csvtomd
 
 def app():
     st.title('Home')
-    with open('markdown/schedule.csv', 'r') as file:
+    with open('assets/schedule.csv', 'r') as file:
         table = csvtomd.csv_to_table(file, ',')
     markdown = csvtomd.md_table(table, padding=2)
     st.markdown(markdown)
