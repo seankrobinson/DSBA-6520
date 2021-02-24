6f2351bd5792df677f3198fbe631d6704892923f
Sean Robinson <Seankrobinson95@gmail.com>
2021-02-24 15:15:28 -0500

    Adds titles for schedule and syllabus pages

--- apps/schedule.py
+++ apps/schedule.py
@@ -2,7 +2,7 @@ import streamlit as st
 import csvtomd
 
 def app():
-    st.title('Home')
+    st.title('Class Schedule')
     with open('assets/schedule.csv', 'r') as file:
         table = csvtomd.csv_to_table(file, ',')
     markdown = csvtomd.md_table(table, padding=2)
--- apps/syllabus.py
+++ apps/syllabus.py
@@ -1,9 +1,7 @@
 import streamlit as st
-import pandas as pd
-import numpy as np
-from data.create_data import create_table
 
 def app():
+    st.title('Syllabus')
     with open('markdown/DSBA_6520_Summer_2021.md') as file:
         markdown = file.read()
     st.markdown(markdown)
